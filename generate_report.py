aa5f9fea59ef115c4e590dd3109a788ceb5e844c
Glib Briia <glib.briia@gmail.com>
2021-02-22 22:44:13 +0000

    Add tests

---
+++ generate_report.py
@@ -0,0 +1,92 @@
+import xmltodict
+import re
+import json
+
+def get_status(robot_status):
+    mapping = {
+        'PASS': "passed",
+        'FAIL': "failed",
+    }
+    return mapping.get(robot_status, "Invalid status")
+
+def get_keyword(kw):
+    if re.match(r"^Given.+", kw):
+        return "Given "
+    if re.match(r"^When.+", kw):
+        return "When "
+    if re.match(r"^Then.+", kw):
+        return "Then "
+    if re.match(r"^And.+", kw):
+        return "And "
+
+def strip_keyword(kw):
+    return re.sub(r"^(Given|When|Then|And)\s", "", kw)
+
+
+def populate_steps(kw):
+    step = {}
+    result = {}
+    step['name'] = strip_keyword(kw['@name'])
+    result['status'] = get_status(kw['status']['@status'])
+    step['result'] = result
+    step['keyword'] = get_keyword(kw['@name'])
+    step['line'] = 0
+    step['match'] = {}
+    return step
+
+
+def populate_elements(test):
+    element = {}
+    steps = []
+    element['name'] = test['@name']
+    element['description'] = test['status']['@status']
+    element['type'] = 'scenario'
+    element['keyword'] = 'Scenario'
+    try:
+        for kw in test['kw']:
+            steps.append(populate_steps(kw))
+    except TypeError:
+        # tests is not iterable
+        steps.append(populate_steps(kw))
+    element['steps'] = steps
+    return element
+
+
+def parse_test(tests, elements):
+    try:
+        for test in tests:
+            elements.append(populate_elements(test))
+    except TypeError:
+        # tests is not iterable
+        elements.append(populate_elements(tests))
+    return elements
+
+
+def parse_suites(suites):
+    data = {}
+    elements = []
+    try:
+        for suite in suites:
+            data['elements'] = parse_test(suite['test'], elements)
+    except TypeError:
+        # suites is not iterable
+        data['elements'] = parse_test(suites['test'], elements)
+    return data
+
+
+with open("output.xml", "r") as myfile:
+    xml_input = myfile.read().replace('\n', '')
+
+    all_data = xmltodict.parse(xml_input)['robot']
+    report = []
+    data = parse_suites(all_data['suite']['suite'])
+    for item in all_data['suite']['suite']['metadata']['item']:
+        if "Feature" == item['@name']:
+            data['name'] =  item['#text']
+    data['description'] = all_data['suite']['suite']['doc']
+    data['id'] = all_data['suite']['suite']['@id']
+    data['keyword'] = "Feature"
+    data['uri'] = all_data['suite']['@source']
+    report.append(data)
+    with open("cucumber.json", "w") as outfile:
+        json.dump(report, outfile)
---
+++ login_tests/login_step_definitions.robot
@@ -0,0 +1,54 @@
+*** Settings ***
+Documentation     A resource file with reusable keywords and variables.
+...
+...               The system specific keywords created here form our own
+...               domain specific language. They utilize keywords provided
+...               by the imported SeleniumLibrary.
+Library           SeleniumLibrary
+
+*** Variables ***
+${SERVER}         localhost:7272
+${BROWSER}        Firefox
+${DELAY}          0
+${VALID USER}     demo
+${VALID PASSWORD}    mode5
+${LOGIN URL}      http://${SERVER}/
+${WELCOME URL}    http://${SERVER}/welcome.html
+${ERROR URL}      http://${SERVER}/error.html
+
+*** Keywords ***
+Open Browser To Login Page
+    Open Browser    ${LOGIN URL}    ${BROWSER}
+    Maximize Browser Window
+    Set Selenium Speed    ${DELAY}
+    Login Page Should Be Open
+
+Login Page Should Be Open
+    Title Should Be    Login Page
+
+Go To Login Page
+    Go To    ${LOGIN URL}
+    Login Page Should Be Open
+
+Input Username
+    [Arguments]    ${username}
+    Input Text    username_field    ${username}
+
+Input Password
+    [Arguments]    ${password}
+    Input Text    password_field    ${password}
+
+Submit Credentials
+    Click Button    login_button
+
+welcome page should be open
+    Location Should Be    ${WELCOME URL}
+    Title Should Be    Welcome Page1
+
+browser is opened to login page
+    Open browser to login page
+
+user "${username}" logs in with password "${password}"
+    Input username    ${username}
+    Input password    ${password}
+    Submit credentials
